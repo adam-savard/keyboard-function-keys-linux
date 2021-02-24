a0edb69ee6d2e290ff4d709b8cf4acbce88264a4
Muhammad Fuad Ardiono <fuad@investree.id>
2021-02-25 02:11:39 +0800

    #4 Add exception to script

--- install.sh
+++ install.sh
@@ -1,13 +1,66 @@
 #!/bin/bash
+export copyServiceException=100
+export setFnModeException=101
+export enableServiceException=102
 
+function try()
+{
+    [[ $- = *e* ]]; SAVED_OPT_E=$?
+    set +e
+}
+
+function throw()
+{
+    exit $1
+}
+
+function catch()
+{
+    export ex_code=$?
+    (( $SAVED_OPT_E )) && set +e
+    return $ex_code
+}
+
+function throwErrors()
+{
+    set -e
+}
+
+function ignoreErrors()
+{
+    set +e
+}
+
+try
+(
 echo "#Copy keychron service to /etc/systemd/service"
-sudo cp ./script/keychron.service /etc/systemd/system/keychron.service
+sudo cp ./script/keychron.service /etc/systemd/system/keychron.service || throw $copyServiceException
 echo "#Success"
-
-sudo /bin/bash -c "echo 0 > /sys/module/hid_apple/parameters/fnmode"
 
 echo "#Enable keychron service to make it run on next boot"
-sudo systemctl enable keychron
+sudo systemctl enable keychron || throw $enableServiceException
 echo "#Success"
 
+sudo /bin/bash -c "echo 0 > /sys/module/hid_apple/parameters/fnmode" || throw $setFnModeException
+
 echo "---Install process finish---"
+)
+catch || {
+     # now you can handle
+    case $ex_code in
+        $copyServiceException)
+            echo "#Failed copy keychron service to /etc/systemd/service"
+        ;;
+        $enableServiceException)
+            echo "Failed enable keychron service"
+        ;;
+        $setFnModeException)
+            echo "Failed set fnmode on hid_apple module"
+        ;;
+        *)
+            echo "An unexpected exception was thrown"
+            throw $ex_code # you can rethrow the "exception" causing the script to exit if not caught
+        ;;
+    esac
+    echo "---Install process failed---"
+}
--- remove.sh
+++ remove.sh
@@ -1,7 +1,41 @@
 #!/bin/bash
 
+#!/bin/bash
+export disableServiceException=100
+export deleteServiceException=101
+
+function try()
+{
+    [[ $- = *e* ]]; SAVED_OPT_E=$?
+    set +e
+}
+
+function throw()
+{
+    exit $1
+}
+
+function catch()
+{
+    export ex_code=$?
+    (( $SAVED_OPT_E )) && set +e
+    return $ex_code
+}
+
+function throwErrors()
+{
+    set -e
+}
+
+function ignoreErrors()
+{
+    set +e
+}
+
+try
+(
 echo "#Disable keychron service before delete service"
-sudo systemctl disable keychron
+sudo systemctl disable keychron || throw $disableServiceException
 echo "#Success"
 
 
@@ -10,3 +44,20 @@ sudo rm /etc/systemd/system/keychron.service
 echo "#Success"
 
 echo "---Remove process finish---"
+)
+catch || {
+     # now you can handle
+    case $ex_code in
+        $disableServiceException)
+            echo "#Failed disable keychron service"
+        ;;
+        $deleteServiceException)
+            echo "Failed delete service"
+        ;;
+        *)
+            echo "An unexpected exception was thrown"
+            throw $ex_code # you can rethrow the "exception" causing the script to exit if not caught
+        ;;
+    esac
+    echo "---Remove process failed---"
+}
